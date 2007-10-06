dc50ee9e6a35787660d9c35e2cb7a36f1cd49a60
Aaron Evans <aaron@aarone.org>
2007-10-06 21:16:15 +0000

    * continuing bulk renaming of classes (should compile and run cleanly now)
    git-svn-id: http://aarone.org/svn/repo/Public/SmugMugExport/trunk@118 146c056e-f425-6d8f-f667-c5eb8a84f541

--- Source/SMExportPlugin.h
+++ Source/SMExportPlugin.h
@@ -1,6 +1,6 @@
 //
-//  SmugmugExport.h
-//  SmugMugExport
+//  SMExportPlugin.h
+//  SMExportPlugin
 //
 //  Created by Aaron Evans on 10/7/06.
 //  Copyright 2006 Aaron Evans. All rights reserved.
--- Source/SMExportPlugin.m
+++ Source/SMExportPlugin.m
@@ -1,6 +1,6 @@
 //
-//  SmugmugExport.m
-//  SmugMugExport
+//  SMExportPlugin.m
+//  SMExportPlugin
 //
 //  Created by Aaron Evans on 10/7/06.
 //  Copyright 2006 Aaron Evans. All rights reserved.
