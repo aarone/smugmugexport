39f8e03e57d649d8831d8fcef69c698454c7fb51
Aaron Evans <aaron@aarone.org>
2008-05-25 14:38:48 +0000

    * improved build/deploy script
    git-svn-id: http://aarone.org/svn/repo/Public/SmugMugExport/trunk@164 146c056e-f425-6d8f-f667-c5eb8a84f541

--- Resources/Installer/createInstaller.sh
+++ Resources/Installer/createInstaller.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
+PROJ_ROOT=../../
 PKG=SmugMugExport.pkg
-DST=../../build/${PKG}
+DST=${PROJ_ROOT}/build/
 DOC=SmugMugExport.pmdoc
+PROJ=${PROJ_ROOT}/SmugMugExport.xcodeproj
 PKG_MAKER=/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker
-VERSION=`/usr/libexec/PlistBuddy ../SmugMugExport-Info.plist -c "Print :CFBundleVersion"`
+INFO_PLIST=${PROJ_ROOT}/Resources/SmugMugExport-Info.plist
+VERSION=`/usr/libexec/PlistBuddy ${INFO_PLIST} -c "Print :CFBundleVersion"`
+SM_ID=`/usr/libexec/PlistBuddy ${INFO_PLIST} -c "Print :CFBundleIdentifier"`
+DISPLAY_NAME=`/usr/libexec/PlistBuddy ${INFO_PLIST} -c "Print :CFBundleDisplayName"`
 ZIP_FILE=${PKG%%.pkg}-${VERSION}.zip
+CONFIG=Release
 
-sudo rm -Rf $DST &&
-sudo $PKG_MAKER --doc $DOC -o $DST --id org.aarone.smugmugexport --version ${VERSION} --title "SmugMugExport" &&
-cd ${DST}/.. &&
+echo "Building SmugMug Release"
+xcodebuild -project ${PROJ}  -target=${DISPLAY_NAME} -configuration=${CONFIG} &&
+echo "Building Distribution from build"
+sudo rm -Rf $DST${PKG} &&
+sudo $PKG_MAKER --doc $DOC -o ${DST}${PKG} --id ${SM_ID} --version ${VERSION} --title ${DISPLAY_NAME} &&
+cd ${DST} &&
 sudo zip -r ${ZIP_FILE} ${PKG} &&
-echo "Installer created: " ${DST} &&
-echo "Zipped installer created: " ${ZIP_FILE} || echo "FAIL!"
+echo "Installer created: " ${DST}${PKG} &&
+echo "Zipped installer created: " ${DST}${ZIP_FILE} || echo "Failed to create distribution!"
 
